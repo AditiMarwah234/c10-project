e8d9a60a6bd049c5db5d6ecf13dae05a389301d0
AditiMarwah234 <104973840+AditiMarwah234@users.noreply.github.com>
2022-05-19 17:45:27 +1000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+var weight = [25, 54, 19, 87, 3]
+function setup() 
+{
+  createCanvas(400,400);
+  var sum = weight[0]+ weight[1] + weight[2] +weight[3] +weight[4] 
+  console.log(sum/weight.length)
+}
+
+function draw() 
+{
+background(51);
+
+}
+
